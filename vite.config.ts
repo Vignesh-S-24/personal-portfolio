d847e028878bb6092fb97dc67486b0a9e786439a
Vignesh-HEPL <159533640+Vignesh-HEPL@users.noreply.github.com>
2025-05-27 12:10:16 +0530

    Initial Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig,} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-  // const env = loadEnv(mode, process.cwd());
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
   return {
     plugins: [react()],
-    // base: env.VITE_BASE_URL || '/ ',
-    base: '/personal-portfolio/',
-
+    base: env.VITE_BASE_URL || '/',
   };
 });
