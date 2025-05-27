1d98773c053c73cf546693efe551b371d2a79f82
Vignesh-HEPL <159533640+Vignesh-HEPL@users.noreply.github.com>
2025-05-27 12:38:00 +0530

    Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+export default defineConfig(() => {
+ 
 
   return {
     plugins: [react()],
-    base: env.VITE_BASE_URL || '/',
+   base: '/personal-portfolio/',
     build: {
       chunkSizeWarningLimit: 800,
       rollupOptions: {
